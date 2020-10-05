f7e880935763f66a5cd8a9412cf16a1b7cbfba87
RicardoQuintero3924 <rikrdoquinvilla3924@gmail.com>
2020-10-05 15:32:33 -0500

    finish from

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
 $(function() {
-    'use strct';
-    var funciona = 'funciona HPTA';
-    alert(funciona);
+    'use strict';
+    //JQUERY PARA CAMBIO DE VISTA DE ROBO A DAÑO
+    // $('.evento .info:first').show();
+    // $('.select a:first').addClass('activo');
+    // $('.select a').on('click', function() {
+    //     $('.select a').removeClass('activo');
+    //     $(this).addClass('activo');
+    //     $('.ocultar').hide();
+    //     $('element.style').removeAttr("display");
+
+    //     var enlace = $(this).attr('href');
+    //     console.log(enlace);
+
+    //     return false;
+    // });
+
+
+
 });
