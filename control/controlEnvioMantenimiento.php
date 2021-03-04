6b420612e39159a17c1f9dee2f60a51b9e3178a0
Ricardo Quintero <rikrdoquinvilla3924@gmail.com>
2021-03-04 17:03:09 -0500

    control envio mantenimiento ok

---
+++ control/controlEnvioMantenimiento.php
@@ -0,0 +1,36 @@
+<?php
+require_once 'modelo/envioMantenimientoM.php';
+
+Class ControlEnvioMantenimiento{
+    private $cnx;
+    
+    public function __construct(){
+        require_once 'conexion/Db.php';
+        try{
+            $this->cnx = Db::conectar();
+        }
+        catch(PDOException $ex){
+            die($ex->getMessage());
+        }
+    }
+    public function registroEnvioMantenimiento($mantenimiento){
+        try{
+            $sql = 'insert into enviomt (tipo_equipo, imei, serial, sedeE, fecha, empresa, ruta, responsable, descripcion) values (?, ?, ?, ?, ?, ?, ?, ?, ?)';
+            $prep = $this->cnx->prepare($sql);
+            $prep->execute([
+                $mantenimiento->GetTipoEquipo(),
+                $mantenimiento->GetImei(),
+                $mantenimiento->GetSerial(),
+                $mantenimiento->GetSedeE(),
+                $mantenimiento->GetFechaE(),
+                $mantenimiento->GetEmpresa(),
+                $mantenimiento->GetRuta(),
+                $mantenimiento->GetResponsableE(),
+                $mantenimiento->GetObservaciones()
+            ]);
+        }catch(PDOException $ex){
+            die($ex->getMessage());
+        }
+    }
+
+}
